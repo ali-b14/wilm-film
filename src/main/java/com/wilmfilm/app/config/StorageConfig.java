5a26ce66c15ee9ba96cb5208adf79c9e563e7c80
ali-b14 <alib714@gmail.com>
2024-05-11 16:23:25 -0400

    Created StorageService service class

--- src/main/java/com/wilmfilm/app/config/StorageConfig.java
+++ src/main/java/com/wilmfilm/app/config/StorageConfig.java
@@ -1,5 +1,9 @@
 package com.wilmfilm.app.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +20,10 @@ public class StorageConfig {
 
     @Value("${cloud.aws.region.static}")
     private String region;
-    //    @Bean
-    //    private AmazonS3 generateS3client(){
-    //        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-    //        return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
-    //    }
+
+    @Bean
+    AmazonS3 generateS3client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
+    }
 }
